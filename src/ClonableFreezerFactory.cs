f7d5e6e203fb31da3af7285881a70241f387936f
Victor Milovanov <lost@losttech.software>
2017-03-26 18:33:54 -0700

    implemented trivial freezer factory for ICopyable

---
+++ src/ClonableFreezerFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace LostTech.App
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public sealed class ClonableFreezerFactory : IFreezerFactory
+    {
+        private ClonableFreezerFactory() { }
+
+        public static IFreezerFactory Instance { get; } = new ClonableFreezerFactory();
+
+        public Func<T, TFreezed> MakeFreezer<T, TFreezed>()
+        {
+            if (!typeof(T).GetTypeInfo().ImplementedInterfaces.Any(@interface => @interface == typeof(ICopyable<TFreezed>)))
+                throw new NotSupportedException($"This factory requires {nameof(T)} to implement {nameof(ICopyable<TFreezed>)}");
+
+            return value => ((ICopyable<TFreezed>)value).Copy();
+        }
+    }
+}
