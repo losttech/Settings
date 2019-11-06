e636baa6d20f3493bb3ac564157fcec8b3eac84b
Victor Milovanov <lost@losttech.software>
2019-11-05 21:11:56 -0800

    updated test project

--- Tests/IntegrationTests.cs
+++ Tests/IntegrationTests.cs
@@ -5,7 +5,6 @@
     using System.Text;
     using System.Threading.Tasks;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using PCLStorage;
 
     [TestClass]
     public class IntegrationTests
